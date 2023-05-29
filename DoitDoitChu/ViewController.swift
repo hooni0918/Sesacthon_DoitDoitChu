7ff2a77989c54a9645afe46d6e619be96a2336ca
hooni <jhlee909090@gmail.com>
2023-05-30 00:15:27 +0900

    git 연결

---
+++ DoitDoitChu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoitDoitChu
+//
+//  Created by 이지훈 on 2023/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
