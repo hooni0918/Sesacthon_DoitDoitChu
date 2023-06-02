c16efe6586bd39dab9091251cd304733e460f721
hooni <jhlee909090@gmail.com>
2023-06-03 00:19:38 +0900

    모달등록
    투두 추가하기 view 등록완료

---
+++ DoitDoitChu/Solo/WriteTodoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WriteTodoViewController.swift
+//  DoitDoitChu
+//
+//  Created by 이지훈 on 2023/06/02.
+//
+
+import UIKit
+
+class WriteTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
