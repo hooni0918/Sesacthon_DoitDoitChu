db972268c06a43133bdf25419e64bb9013acf7b5
hooni <jhlee909090@gmail.com>
2023-06-05 16:11:31 +0900

    segment + pageVC 성공

---
+++ DoitDoitChu/History/SelectCharaterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectCharaterViewController.swift
+//  DoitDoitChu
+//
+//  Created by 이지훈 on 2023/06/05.
+//
+
+import UIKit
+
+class SelectCharaterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
