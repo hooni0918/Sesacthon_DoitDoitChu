9e9c3bc27d161bfd9a99219f4e32bb8f2efe9132
hooni <jhlee909090@gmail.com>
2023-06-07 12:51:33 +0900

    나의성과 드롭다운

--- DoitDoitChu/History/MyTodoViewController.swift
+++ DoitDoitChu/History/MyTodoViewController.swift
@@ -18,33 +18,40 @@ class MyTodoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 메뉴 아이템 정의
-                let menuItem1 = UIAction(title: "피자") { action in
-                    print("피자 선택")
-                }
-                let menuItem2 = UIAction(title: "치킨") { action in
-                    print("치킨 선택")
-                }
-                let menuItem3 = UIAction(title: "족발보쌈") { action in
-                    print("족발보쌈 선택")
-                }
-                let menuItem4 = UIAction(title: "치즈돈까스") { action in
-                    print("치즈돈까스 선택")
-                }
-                let menuItem5 = UIAction(title: "햄버거") { action in
-                    print("햄버거 선택")
-                }
-                
-                // 메뉴 생성
-                let menu = UIMenu(title: "", children: [menuItem1, menuItem2, menuItem3, menuItem4, menuItem5])
-                
-                // 버튼에 메뉴 할당
-                DropAllBtn.menu = menu
-                // 버튼 클릭시 메뉴가 나타나게 설정
-                DropAllBtn.showsMenuAsPrimaryAction = true
         
-        New.menu = menu
-        New.showsMenuAsPrimaryAction = true
+        let menuItem1 = UIAction(title: "전체") { action in
+                print("전체 선택")
+            }
+            let menuItem2 = UIAction(title: "개인투두") { action in
+                print("개인투두 선택")
+            }
+            let menuItem3 = UIAction(title: "팀투두") { action in
+                print("팀투두 선택")
+            }
+
+            // 메뉴 생성
+            let menu1 = UIMenu(title: "", children: [menuItem1, menuItem2, menuItem3])
+
+            // 버튼에 메뉴 할당
+            DropAllBtn.menu = menu1
+            // 버튼 클릭시 메뉴가 나타나게 설정
+            DropAllBtn.showsMenuAsPrimaryAction = true
+
+            // New 버튼 메뉴 아이템 정의
+            let menuItem4 = UIAction(title: "최신순") { action in
+                print("최신순 선택")
+            }
+            let menuItem5 = UIAction(title: "오래된순") { action in
+                print("오래된순 선택")
+            }
+
+            // 메뉴 생성
+            let menu2 = UIMenu(title: "", children: [menuItem4, menuItem5])
+
+            // 버튼에 메뉴 할당
+            New.menu = menu2
+            // 버튼 클릭시 메뉴가 나타나게 설정
+            New.showsMenuAsPrimaryAction = true
         
             }
     
